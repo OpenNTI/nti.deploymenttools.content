6d7e074fd1946fac34d1ad42350e0518d0628845
Sean Jones <sean.jones@nextthought.com>
2013-08-27 16:05:07 -0500

    Add a content metadata caching catalog.
    The new build_content_catalog tool will build the intial catalog and and will update the catalog if changes are made to the content store outside of the included tools. All existing tools or library calls that the tools use have been updated to support the catalog. The real benefit of the catalog can be seen with either update_content or publish_content. Both tools are significantly faster now that the metadata is cached. However, there is still one major section in publish_content that needs to be optimized, but that is for another day.

--- src/nti/deploymenttools/content/__init__.py
+++ src/nti/deploymenttools/content/__init__.py
@@ -8,9 +8,15 @@ import importlib
 import json
 import os
 import shutil
+import sqlite3
 import subprocess
 import sys
 import tarfile
+
+import logging
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
 
 def compare_files( file_1, file_2 ):
     """Computes the hashes for file_1 and file_2 and returns if they are equal or not"""
@@ -97,6 +103,227 @@ def get_published_content_metadata( config, s3conn, content_title ):
         content = json.loads(key.get_contents_as_string())
     return content
 
+###############
+# This block supports builds and manages the content catalog
+###############
+def _initialize_db( dbfile ):
+    conn = sqlite3.connect( dbfile )
+
+    cursor = conn.cursor()
+    cursor.execute('''CREATE TABLE records (name TEXT, version INT, builder TEXT, build_time INT, indexer TEXT, index_time INT, archive TEXT, PRIMARY KEY(name, version))''')
+    cursor.execute('''CREATE TABLE state (recordId INT, state TEXT, PRIMARY KEY(recordId, state))''')
+    conn.commit()
+
+    return conn
+
+def _write_catalog( conn, catalog ):
+    cursor = conn.cursor()
+    for entry in catalog:
+        entry = catalog[entry]
+        # Test if the record is already in the DB
+        cursor.execute('SELECT COUNT(*) FROM records WHERE name=? and version=?', (entry['name'], int(entry['version'])))
+        if int(cursor.fetchone()[0]):
+            logger.debug('%s, version %s, is already in the database.' % (entry['name'], entry['version']))
+        else:
+            cursor.execute('INSERT INTO records VALUES (?,?,?,?,?,?,?)', (entry['name'], int(entry['version']), entry['builder'], int(entry['build_time']), entry['indexer'], int(entry['index_time']), entry['archive']))
+
+        cursor.execute('SELECT _rowid_ FROM records WHERE name=? and version=?', (entry['name'], int(entry['version'])))
+        row_id = cursor.fetchone()[0]
+        for state in entry['state']:
+            cursor.execute('SELECT COUNT(*) FROM state WHERE recordId=? and state=?', (row_id, state))
+            if int(cursor.fetchone()[0]) == 0:
+                cursor.execute('INSERT INTO state VALUES (?,?)', (row_id, state))
+
+    conn.commit()
+
+def _read_catalog( conn ):
+    catalog = {}
+    cursor = conn.cursor()
+    
+    # Fetch the records
+    cursor.execute('SELECT name, version, builder, build_time, indexer, index_time, archive, _rowid_ FROM records ORDER BY version DESC, name')
+    records = cursor.fetchall()
+    for record in records:
+        key = '-'.join([record[0], unicode(record[1])])
+        logger.debug('Read %s from catalog' % (key))
+        catalog[key] = {'name': record[0], 'version': record[1], 'builder': record[2], 'build_time': record[3], 'indexer': record[4], 'index_time': record[5], 'archive': record[6], 'state': []}
+        cursor.execute('SELECT state FROM state WHERE recordId=?', (record[7],))
+        states = cursor.fetchall()
+        for state in states:
+            catalog[key]['state'].append(state[0])
+    return catalog
+
+def _is_content_package( file ):
+    return (os.path.isfile(file) and '.tgz' in file)
+
+def _is_valid_file_or_link( filename ):
+    return ((os.path.isfile(filename) and not os.path.islink(filename)) or (os.path.islink(filename) and os.path.exists(os.path.join(os.path.dirname(filename), os.readlink(filename)))))
+
+def _build_catalog(conn, content_store):
+    catalog = {}
+    for root, dirs, files in os.walk(content_store):
+        for file in files:
+            content_file = os.path.join(root,file)
+            if _is_content_package(content_file) and _is_valid_file_or_link(content_file):
+                logger.debug('Processesing %s' % (file,))
+                state = os.path.split(root)[1]
+                record = get_content_metadata( os.path.join( root, file ))
+                if os.path.islink(content_file):
+                    linkpath = os.readlink(content_file)
+                    if not os.path.isabs(linkpath):
+                        linkpath = os.path.normpath(os.path.join(os.path.dirname(content_file), linkpath))
+                    record['archive'] = os.path.relpath(linkpath, content_store)
+                else:
+                    record['archive'] = os.path.relpath(content_file, content_store)
+                record['state'] = []
+                key = '-'.join([record['name'], record['version']])
+                if key not in catalog.keys():
+                    catalog[key] = record
+                catalog[key]['state'].append(state)
+
+    _write_catalog(conn, catalog)
+
+    return catalog
+
+def _clean_catalog(conn, content_store):
+    catalog = _read_catalog( conn )
+    
+    for entry in catalog.values():
+        if _is_valid_file_or_link(os.path.join(content_store, entry['archive'])):
+            logger.debug('%s is present' % (os.path.join(content_store, entry['archive']),))
+            
+            base, file = os.path.split(entry['archive'])
+            for state in entry['state']:
+                if state == base:
+                    continue
+                if _is_valid_file_or_link(os.path.join(content_store,state, file)):
+                    logger.debug('%s exists for %s' % (state, file))
+                else:
+                    logger.warning('%s missing for %s. Removing.' % (state, file))
+                    entry['state'].remove(state)
+                    update_catalog(conn, [entry])
+
+        else:
+            logger.warn('%s is missing. We should remove this from the catalog' % (os.path.join(content_store, entry['archive']),))
+            cursor = conn.cursor()
+            cursor.execute('SELECT _rowid_ FROM records WHERE name=? and version=?', (entry['name'], int(entry['version'])))
+            row_id = cursor.fetchone()[0]
+            for state in entry['state']:
+                cursor.execute('DELETE FROM state WHERE recordId=?', (row_id,))
+            cursor.execute('DELETE FROM records WHERE _rowid_=?', (row_id,))
+            conn.commit()
+
+def _open_catalog( content_store ):
+    if _is_content_package(content_store):
+        logger.warn('The content-store is a content package.')
+        return
+
+    dbfile = os.path.join(content_store, 'catalog.db')
+
+    conn = None
+    existing = True
+    if not os.path.exists(dbfile):
+        conn = _initialize_db(dbfile)
+        existing = False
+    else:
+        conn = sqlite3.connect(dbfile)
+
+    return (conn, existing)
+
+def get_catalog( content_store ):
+    conn, existing = _open_catalog( content_store )
+
+    if existing:
+        catalog = _read_catalog(conn)
+    else:
+        catalog = _build_catalog(conn, content_store)
+
+    return catalog
+
+def get_from_catalog( content_store, title='', version='' ):
+    conn, existing = _open_catalog( content_store )
+
+    if not existing:
+        _build_catalog(conn, content_store)
+
+    cursor = conn.cursor()
+    if version and title:
+        cursor.execute('SELECT name, version, builder, build_time, indexer, index_time, archive, _rowid_  FROM records WHERE name=? AND version=?', (title, version))
+    elif version:
+        cursor.execute('SELECT name, version, builder, build_time, indexer, index_time, archive, _rowid_  FROM records WHERE version=?', (version,))
+    elif title:
+        cursor.execute('SELECT name, version, builder, build_time, indexer, index_time, archive, _rowid_  FROM records WHERE name=?', (title,))
+
+    catalog = []
+    records = cursor.fetchall()
+    for record in records:
+        entry = {'name': record[0], 'version': unicode(record[1]), 'builder': record[2], 'build_time': unicode(record[3]), 'indexer': record[4], 'index_time': unicode(record[5]), 'archive': record[6], 'state': []}
+        entry['archive'] = os.path.join(content_store, entry['archive'])
+        cursor.execute('SELECT state FROM state WHERE recordId=?', (record[7],))
+        states = cursor.fetchall()
+        for state in states:
+            entry['state'].append(state[0])
+        catalog.append(entry)
+
+    conn.close()
+
+    return catalog
+
+def get_latest_from_catalog( content_store, category='testing', title='' ):
+    conn, existing = _open_catalog( content_store )
+
+    if not existing:
+        _build_catalog(conn, content_store)
+
+    cursor = conn.cursor()
+    if title:
+        cursor.execute('SELECT name, MAX(version), builder, build_time, indexer, index_time, archive  FROM records JOIN state ON records._rowid_ = state.recordId WHERE state=? AND name=? GROUP BY name', (category, title))
+    else:
+        cursor.execute('SELECT name, MAX(version), builder, build_time, indexer, index_time, archive  FROM records JOIN state ON records._rowid_ = state.recordId WHERE state=? GROUP BY name', (category,))
+
+    catalog = []
+    records = cursor.fetchall()
+    for record in records:
+        entry = {'name': record[0], 'version': unicode(record[1]), 'builder': record[2], 'build_time': unicode(record[3]), 'indexer': record[4], 'index_time': unicode(record[5]), 'archive': record[6], 'state': [category]}
+        entry['archive'] = os.path.join(content_store, entry['archive'])
+        catalog.append(entry)
+
+    conn.close()
+
+    return catalog
+
+def update_catalog( content_store, content_list):
+    conn, existing = _open_catalog( content_store )
+
+    #if not existing:
+    #    _build_catalog(conn, content_store)
+
+    cursor = conn.cursor()
+    
+    for entry in content_list:
+        # Determine if the entry is already in the DB.
+        cursor.execute('SELECT COUNT(*) FROM records WHERE name=? AND version=?', (entry['name'], int(entry['version'])))
+        if int(cursor.fetchone()[0]) == 0:
+            logger.info('Adding %s, version %s, to the database.' % (entry['name'], entry['version']))
+            cursor.execute('INSERT INTO records VALUES (?,?,?,?,?,?,?)', (entry['name'], int(entry['version']), entry['builder'], int(entry['build_time']), entry['indexer'], int(entry['index_time']), entry['archive']))
+        else:
+            logger.info('%s, version %s, is already in the database.' % (entry['name'], entry['version']))
+
+        cursor.execute('SELECT _rowid_ FROM records WHERE name=? AND version=?', (entry['name'], int(entry['version'])))
+        row_id = cursor.fetchone()[0]
+        for state in entry['state']:
+            cursor.execute('SELECT COUNT(*) FROM state WHERE recordId=? AND state=?', (row_id, state))
+            if int(cursor.fetchone()[0]) == 0:
+                cursor.execute('INSERT INTO state VALUES (?,?)', (row_id, state))
+        cursor.execute('SELECT state FROM state WHERE recordId=?', (row_id,))
+        states = cursor.fetchall()
+        for state in states:
+            if state[0] not in entry['state']:
+                cursor.execute('DELETE FROM state WHERE recordId=? AND state=?', (row_id, state[0]))
+
+    conn.commit()
+    conn.close()
+
 def _get_content( content_store='.', prefix='testing', title='', version='', all_versions=False ):
     """This method retrieves content from the local filesystem content store. 
     The name variable specifies the desired content title. The version 
@@ -105,35 +332,12 @@ def _get_content( content_store='.', prefix='testing', title='', version='', all
     for each content title found in the local store.
     NOTE: Does not yet return all mathcing content when requested."""
 
-    archives = {}
-    # Get the items in the directory content_path
-    for item in os.listdir( os.path.join( content_store, prefix ) ):
-        filename = os.path.join( content_store, prefix, item )
-        # The item is a file and contains .tgz assume it is a content archive.  This is potentially dangerous.
-        if os.path.isfile( filename ) and '.tgz' in filename:
-            _t = get_content_metadata( filename )
-
-            if title is not '' and title != _t['name']:
-                continue
-
-            if title is not '' and version is not '':
-                if title == _t['name'] and version == _t['version']:
-                    archives[_t['name']] = _t
-                    break
-            else:
-                if _t['name'] in archives:
-                    # Check and see if the current item is the newest archive for that content.
-                    # If this is the newest, update the tracking list
-                    if archives[_t['name']]['version'] < _t['version']:
-                        archives[_t['name']] = _t
-                else:
-                    # We have not seen this content before so add it.
-                    archives[_t['name']] = _t
-
-    # Build a simple list to return to the caller
+    # Determine if we are getting the latest content or a particular version
     content_list = []
-    for archive in archives:
-        content_list.append( archives[archive] )
+    if version is '':
+        content_list = get_latest_from_catalog( content_store, category=prefix, title=title )
+    else:
+        content_list = get_from_catalog( content_store, title=title, version=version )
 
     return content_list
 
@@ -183,6 +387,10 @@ def put_content( config, content_list=None, dest='testing' ):
     if os.path.exists( config['content-store'] ):
         for content in content_list:
             move_content( config, content, dest )
+            if not hasattr(content, 'state'):
+                content['state'] = []
+            content['state'].append(dest);
+    update_catalog(config['content-store'], content_list)
 
 def set_default_root_sharing( content, environment = 'prod' ):
     filename = os.path.join( os.path.dirname( __file__ ), 'default_sharing.json' )
---
+++ src/nti/deploymenttools/content/build_catalog.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+from __future__ import unicode_literals, print_function
+
+from . import _open_catalog
+from . import _build_catalog
+from . import _clean_catalog
+
+import argparse
+import ConfigParser
+import os
+
+import logging
+
+logger = logging.getLogger('content')
+logger.setLevel(logging.INFO)
+log_handler = logging.StreamHandler()
+log_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+log_handler.setLevel(logging.INFO)
+logger.addHandler(log_handler)
+
+DEFAULT_CONFIG_FILE='~/etc/nti_util_conf.ini'
+
+def _parse_args():
+    # Parse command line args
+    arg_parser = argparse.ArgumentParser( description="Content Catalog Builder" )
+    arg_parser.add_argument( '-c', '--config', default=DEFAULT_CONFIG_FILE,
+                             help="Configuration file. The default is: %s" % DEFAULT_CONFIG_FILE )
+    arg_parser.add_argument( '-p', '--contentstore', default='', 
+                             help="The directory containing content store" )
+    return arg_parser.parse_args()
+
+def main():    
+    args = _parse_args()
+    content_store = os.path.expanduser(args.contentstore)
+
+    # Read the config file
+    configfile = ConfigParser.SafeConfigParser()
+    configfile.read( os.path.expanduser( args.config ) )
+
+    # Build the effective config. This will be important when all of the command line arguments are hooked up.
+    config = {}
+    config['content-store'] = content_store or configfile.get('local', 'content-store')
+
+    conn, existing = _open_catalog(config['content-store'])
+    if existing:
+        print('Rebuilding the catalog for %s' % (config['content-store'],))
+    else:
+        print('Building the catalog for %s' % (config['content-store'],))
+
+    if existing:
+        print('Cleaning exising catalog prior to rebuilding it.')
+        _clean_catalog(conn, config['content-store'])
+
+    _build_catalog(conn, config['content-store'])
+
+
+if __name__ == '__main__': # pragma: no cover
+        main()
