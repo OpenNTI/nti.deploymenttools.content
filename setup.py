ef6b9bc8001bacd56536faf9a1d706bb0f030676
Carlos Sanchez <papachoco@gmail.com>
2017-10-13 00:39:13 -0500

    add supporting files

--- setup.py
+++ setup.py
@@ -1,46 +1,67 @@
-#!/usr/bin/env python
+import codecs
 from setuptools import setup, find_packages
 
-VERSION = '1.0.0rc1'
-
 entry_points = {
-	'console_scripts': [
-		'nti_backup_course = nti.deploymenttools.content.backup_course_bundle:main',
-		'nti_copy_content_package = nti.deploymenttools.content.copy_content_package:main',
-		'nti_copy_course = nti.deploymenttools.content.copy_course:main',
-		'nti_remote_render = nti.deploymenttools.content.remote_render:main',
-		'nti_render_content = nti.deploymenttools.content.render:main',
-		'nti_restore_course = nti.deploymenttools.content.restore_course_bundle:main',
-	]
+    'console_scripts': [
+        'nti_backup_course = nti.deploymenttools.content.backup_course_bundle:main',
+        'nti_copy_content_package = nti.deploymenttools.content.copy_content_package:main',
+        'nti_copy_course = nti.deploymenttools.content.copy_course:main',
+        'nti_remote_render = nti.deploymenttools.content.remote_render:main',
+        'nti_render_content = nti.deploymenttools.content.render:main',
+        'nti_restore_course = nti.deploymenttools.content.restore_course_bundle:main',
+    ]
 }
 
+TESTS_REQUIRE = [
+    'nti.testing',
+    'zope.testrunner',
+]
+
+
+def _read(fname):
+    with codecs.open(fname, encoding='utf-8') as f:
+        return f.read()
+
+
 setup(
-	name = 'nti.deploymenttools.content',
-	version = VERSION,
-	keywords = 'deployment tools',
-	author = 'Sean Jones',
-	author_email = 'sean.jones@nextthought.com',
-	description = 'NextThought Platform Deployment Tools',
-	long_description = 'Dataserver README',
-	classifiers=[
-		"Development Status :: 4 - Beta",
-		"Intended Audience :: Developers :: Education",
-		"Operating System :: OS Independent",
-		"Programming Language :: Python :: 2.7",
-		"Framework :: Pylons :: ZODB :: Pyramid",
-		"Internet :: WWW/HTTP",
-		"Natural Language :: English",
-		"Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-	],
-	install_requires = [
-		'setuptools',
-		'boto',
-		'nti.contentrendering',
-	],
-	packages = find_packages( 'src' ),
-	package_dir = {'': 'src'},
-	include_package_data = True,
-	namespace_packages=['nti', 'nti.deploymenttools'],
-	zip_safe = False,
-	entry_points = entry_points
-	)
+    name='nti.deploymenttools.content',
+    version=_read('version.txt').strip(),
+    author='Sean Jones',
+    author_email='sean.jones@nextthought.com',
+    description='NextThought Platform Deployment Tools',
+    long_description=(_read('README.rst') + '\n\n' + _read("CHANGES.rst")),
+    license='Apache',
+    keywords='deployment tools',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        "Development Status :: 4 - Beta",
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    url="https://github.com/NextThought/nti.deploymenttools.content",
+    zip_safe=True,
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    namespace_packages=['nti', 'nti.deploymenttools'],
+    tests_require=TESTS_REQUIRE,
+    install_requires=[
+        'setuptools',
+        'boto',
+        'nti.contentrendering',
+        'six',
+    ],
+    extras_require={
+        'test': TESTS_REQUIRE,
+        'docs': [
+            'Sphinx',
+            'repoze.sphinx.autointerface',
+            'sphinx_rtd_theme',
+        ],
+    },
+    entry_points=entry_points,
+)
