244a7811a42ec9a28c26225be3ed5ab6ad5f82e3
Carlos Sanchez <papachoco@gmail.com>
2017-10-13 15:06:16 -0500

    pep8; pylint

--- src/nti/deploymenttools/content/backup_course_bundle.py
+++ src/nti/deploymenttools/content/backup_course_bundle.py
@@ -1,45 +1,59 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
 
-from argparse import ArgumentParser
-from getpass import getpass
-
-from nti.deploymenttools.content import configure_logging
-from nti.deploymenttools.content import export_course
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
 import logging
+from getpass import getpass
+from argparse import ArgumentParser
+
 import requests
 
-logger = logging.getLogger('nti_backup_course')
-logging.captureWarnings(True)
+from nti.deploymenttools.content import export_course
+from nti.deploymenttools.content import configure_logging
 
 UA_STRING = 'NextThought Course Backup Utility'
 
-def backup_course(course_ntiid, source_host, username, cleanup=True):
+logger = __import__('logging').getLogger(__name__)
+logging.captureWarnings(True)
+
+
+def backup_course(course_ntiid, source_host, username, unused_cleanup=True):
     course_archive = None
     try:
-        password = getpass('Password for %s@%s: ' % (username, source_host))
-        logger.info("Backing up %s from %s" % (course_ntiid, source_host))
-        course_archive = export_course( course_ntiid, source_host, username, password, UA_STRING, backup=True )
-        logger.info('Course %s backed up at %s.' % (course_ntiid,course_archive))
-
+        password = getpass('Password for %s@%s:' % (username, source_host))
+        logger.info("Backing up %s from %s", course_ntiid, source_host)
+        course_archive = export_course(course_ntiid, source_host, username, 
+                                       password, UA_STRING, backup=True)
+        logger.info('Course %s backed up at %s.',
+                    course_ntiid, course_archive)
     except requests.exceptions.HTTPError as e:
         logger.error(e)
 
+
 def _parse_args():
-    arg_parser = ArgumentParser( description=UA_STRING )
-    arg_parser.add_argument( '-n', '--ntiid', dest='ntiid',
-                             help="NTIID of the course to copy." )
-    arg_parser.add_argument( '-s', '--source-server', dest='source_host',
-                             help="Source server." )
-    arg_parser.add_argument( '-u', '--user', dest='user',
-                             help="User to authenticate with the server." )
-    arg_parser.add_argument( '-v', '--verbose', dest='loglevel', action='store_const', const=logging.DEBUG,
-                             help="Print debugging logs." )
-    arg_parser.add_argument( '-q', '--quiet', dest='loglevel', action='store_const', const=logging.WARNING,
-                             help="Print warning and error logs only." )
-    arg_parser.add_argument( '--no-cleanup', dest='no_cleanup', action='store_false', default=True,
-                             help="Do not cleanup process files." )
+    arg_parser = ArgumentParser(description=UA_STRING)
+    arg_parser.add_argument('-n', '--ntiid', dest='ntiid',
+                            help="NTIID of the course to copy.")
+    arg_parser.add_argument('-s', '--source-server', dest='source_host',
+                            help="Source server.")
+    arg_parser.add_argument('-u', '--user', dest='user',
+                            help="User to authenticate with the server.")
+    arg_parser.add_argument('-v', '--verbose', dest='loglevel', action='store_const', 
+                            const=logging.DEBUG, help="Print debugging logs.")
+    arg_parser.add_argument('-q', '--quiet', dest='loglevel', action='store_const', 
+                            const=logging.WARNING,
+                            help="Print warning and error logs only.")
+    arg_parser.add_argument('--no-cleanup', dest='no_cleanup', action='store_false', 
+                            default=True,
+                            help="Do not cleanup process files.")
     return arg_parser.parse_args()
+
 
 def main():
     # Parse command line args
@@ -48,10 +62,11 @@ def main():
     loglevel = args.loglevel or logging.INFO
     configure_logging(level=loglevel)
 
-    backup_course( args.ntiid,
-                 args.source_host,
-                 args.user,
-                 cleanup=args.no_cleanup )
+    backup_course(args.ntiid,
+                  args.source_host,
+                  args.user,
+                  cleanup=args.no_cleanup)
 
-if __name__ == '__main__': # pragma: no cover
-        main()
+
+if __name__ == '__main__':  # pragma: no cover
+    main()
--- src/nti/deploymenttools/content/copy_course.py
+++ src/nti/deploymenttools/content/copy_course.py
@@ -1,50 +1,66 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
 
-from argparse import ArgumentParser
-from getpass import getpass
-from isodate import datetime_isoformat
-from isodate import parse_datetime
-from shutil import rmtree
-from tempfile import mkdtemp
-from zipfile import ZipFile
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
-from nti.deploymenttools.content import archive_directory
-from nti.deploymenttools.content import configure_logging
-from nti.deploymenttools.content import download_rendered_content
-from nti.deploymenttools.content import export_course
-from nti.deploymenttools.content import import_course
-from nti.deploymenttools.content import upload_rendered_content
-
-import logging
 import os
+import logging
+from shutil import rmtree
+from getpass import getpass
+from zipfile import ZipFile
+from tempfile import mkdtemp
+from argparse import ArgumentParser
+
 import requests
 
 import simplejson as json
 
-logger = logging.getLogger('nti_copy_course')
+from isodate import parse_datetime
+from isodate import datetime_isoformat
+
+from nti.deploymenttools.content import export_course
+from nti.deploymenttools.content import import_course
+from nti.deploymenttools.content import archive_directory
+from nti.deploymenttools.content import configure_logging
+from nti.deploymenttools.content import upload_rendered_content
+from nti.deploymenttools.content import download_rendered_content
+
+logger = __import__('logging').getLogger(__name__)
 logging.captureWarnings(True)
 
 UA_STRING = 'NextThought Course Copy Utility'
 
+
 def _get_content_packages(course_archive):
-    content_packages = []
     with ZipFile(course_archive, 'r') as archive:
         bundle_metadata = json.load(archive.open('bundle_meta_info.json'))
         content_packages = bundle_metadata['ContentPackages']
     return content_packages
 
+
 def _get_provider_id(course_archive):
-    provider_id = ''
     with ZipFile(course_archive, 'r') as archive:
         course_info = json.load(archive.open('course_info.json'))
         provider_id = course_info['id']
     return provider_id
 
-def _update_course_archive( course_archive, provider_id, start_date, end_date ):
-    modified_course_archive = os.path.splitext(course_archive)
-    modified_course_archive = modified_course_archive[0] + '_modified' + modified_course_archive[1]
+
+def _remove_path(path):
+    if path and os.path.exists(path):
+        rmtree(path)
+
+
+def _update_course_archive(course_archive, provider_id, start_date, end_date):
     temp_dir = mkdtemp()
 
+    modified_course_archive = os.path.splitext(course_archive)
+    modified_course_archive = modified_course_archive[0] + \
+        '_modified' + modified_course_archive[1]
     try:
         with ZipFile(course_archive, 'r') as archive:
             for name in archive.namelist():
@@ -56,90 +72,104 @@ def _update_course_archive( course_archive, provider_id, start_date, end_date ):
 
             if provider_id:
                 course_info['id'] = provider_id
+
             if start_date:
-                course_info['startDate'] = datetime_isoformat(parse_datetime(start_date))
+                start_date = parse_datetime(start_date)
+                course_info['startDate'] = datetime_isoformat(start_date)
+
             if end_date:
-                course_info['endDate'] = datetime_isoformat(parse_datetime(end_date))
+                end_date = parse_datetime(end_date)
+                course_info['endDate'] = datetime_isoformat(end_date)
 
         with open(os.path.join(temp_dir, 'course_info.json'), 'w') as fp:
             json.dump(course_info, fp)
 
-        archive_directory( temp_dir, modified_course_archive )
+        archive_directory(temp_dir, modified_course_archive)
     finally:
-        if os.path.exists( temp_dir ):
-            rmtree( temp_dir )
-
+        _remove_path(temp_dir)
     return modified_course_archive
 
-def copy_course(course_ntiid, source_host, dest_host, username, site_library, admin_level, provider_id=None, start_date=None, end_date=None, cleanup=True):
+
+def copy_course(course_ntiid, source_host, dest_host, username, site_library,
+                admin_level, provider_id=None, start_date=None, end_date=None, cleanup=True):
+    cwd = os.getcwd()
     course_archive = None
     content_archives = []
     working_dir = mkdtemp()
-    cwd = os.getcwd()
     try:
-        logger.info('Using %s as the working directory' % (working_dir,))
         os.chdir(working_dir)
+        logger.info('Using %s as the working directory', working_dir)
         password = getpass('Password for %s@%s: ' % (username, source_host))
-        logger.info("Exporting %s from %s" % (course_ntiid, source_host))
-        course_archive = export_course( course_ntiid, source_host, username, password, UA_STRING )
+        logger.info("Exporting %s from %s", course_ntiid, source_host)
+        course_archive = export_course(course_ntiid, source_host,
+                                       username, password, UA_STRING)
         if source_host != dest_host:
-            for content_package in _get_content_packages( course_archive ):
-                logger.info("Downloading content package %s" % (content_package,))
-                content_archive = download_rendered_content( content_package, source_host, username, password, UA_STRING )
-                content_archives.append((content_package,content_archive))
+            for content_package in _get_content_packages(course_archive):
+                logger.info("Downloading content package %s", content_package)
+                content_archive = download_rendered_content(content_package, source_host,
+                                                            username, password, UA_STRING)
+                content_archives.append((content_package, content_archive))
 
             password = getpass('Password for %s@%s: ' % (username, dest_host))
 
             for content_archive in content_archives:
-                logger.info("Uploading content package %s" % (content_archive[0],))
-                upload_rendered_content( content_archive[1], dest_host, username, password, site_library, UA_STRING )
+                logger.info("Uploading content package %s", content_archive[0])
+                upload_rendered_content(content_archive[1], dest_host,
+                                        username, password, site_library, UA_STRING)
 
         # Update course metadata with supplied information
-        provider_id = provider_id or _get_provider_id( course_archive )
-        course_archive = _update_course_archive( course_archive, provider_id, start_date, end_date )
+        provider_id = provider_id or _get_provider_id(course_archive)
+        course_archive = _update_course_archive(course_archive, provider_id,
+                                                start_date, end_date)
 
         # TODO: Check if admin level exists on dest server, if not, create it.
-        logger.info("Importing %s to %s" % (course_ntiid, dest_host))
-        course = import_course( course_archive, dest_host, username, password, site_library, admin_level, provider_id, UA_STRING)
-        logger.info('Course imported sucessfully as %s.' % (course['Course']['NTIID'],))
-        
+        logger.info("Importing %s to %s", course_ntiid, dest_host)
+        course = import_course(course_archive, dest_host, username,
+                               password, site_library, admin_level, provider_id, UA_STRING)
+        logger.info('Course imported sucessfully as %s.',
+                    course['Course']['NTIID'])
 
     except requests.exceptions.HTTPError as e:
         logger.error(e)
     finally:
         os.chdir(cwd)
-        # Clean-up
         if cleanup:
-            if working_dir and os.path.exists( working_dir ):
-                rmtree(working_dir)
+            _remove_path(working_dir)
+
 
 def _parse_args():
-    arg_parser = ArgumentParser( description=UA_STRING )
-    arg_parser.add_argument( '-n', '--ntiid', dest='ntiid',
-                             help="NTIID of the course to copy." )
-    arg_parser.add_argument( '-s', '--source-server', dest='source_host',
-                             help="Source server." )
-    arg_parser.add_argument( '-d', '--dest-server', dest='dest_host',
-                             help="Destination server." )
-    arg_parser.add_argument( '-u', '--user', dest='user',
-                             help="User to authenticate with the server." )
-    arg_parser.add_argument( '-a', '--admin-level', dest='admin_level', default='DefaultAPICopied',
-                             help="Specifies the organizational admin level for the course." )
-    arg_parser.add_argument( '-p', '--provider-id', dest='provider_id',
-                             help="Custom provider id for the copied course." )
-    arg_parser.add_argument( '--start-date', dest='start_date',
-                             help="Start date for the copied course." )
-    arg_parser.add_argument( '--end-date', dest='end_date',
-                             help="End date for the copied course." )
-    arg_parser.add_argument( '--site-library', dest='site_library',
-                             help="Site library to add content to. Defaults to the hostname of the destination server." )
-    arg_parser.add_argument( '-v', '--verbose', dest='loglevel', action='store_const', const=logging.DEBUG,
-                             help="Print debugging logs." )
-    arg_parser.add_argument( '-q', '--quiet', dest='loglevel', action='store_const', const=logging.WARNING,
-                             help="Print warning and error logs only." )
-    arg_parser.add_argument( '--no-cleanup', dest='no_cleanup', action='store_false', default=True,
-                             help="Do not cleanup process files." )
+    arg_parser = ArgumentParser(description=UA_STRING)
+    arg_parser.add_argument('-n', '--ntiid', dest='ntiid',
+                            help="NTIID of the course to copy.")
+    arg_parser.add_argument('-s', '--source-server', dest='source_host',
+                            help="Source server.")
+    arg_parser.add_argument('-d', '--dest-server', dest='dest_host',
+                            help="Destination server.")
+    arg_parser.add_argument('-u', '--user', dest='user',
+                            help="User to authenticate with the server.")
+    arg_parser.add_argument('-a', '--admin-level', dest='admin_level',
+                            default='DefaultAPICopied',
+                            help="Specifies the organizational admin level for the course.")
+    arg_parser.add_argument('-p', '--provider-id', dest='provider_id',
+                            help="Custom provider id for the copied course.")
+    arg_parser.add_argument('--start-date', dest='start_date',
+                            help="Start date for the copied course.")
+    arg_parser.add_argument('--end-date', dest='end_date',
+                            help="End date for the copied course.")
+    arg_parser.add_argument('--site-library', dest='site_library',
+                            help="Site library to add content to. Defaults to the hostname of the destination server.")
+    arg_parser.add_argument('-v', '--verbose', dest='loglevel',
+                            action='store_const',
+                            const=logging.DEBUG,
+                            help="Print debugging logs.")
+    arg_parser.add_argument('-q', '--quiet', dest='loglevel', action='store_const',
+                            const=logging.WARNING,
+                            help="Print warning and error logs only.")
+    arg_parser.add_argument('--no-cleanup', dest='no_cleanup', action='store_false',
+                            default=True,
+                            help="Do not cleanup process files.")
     return arg_parser.parse_args()
+
 
 def main():
     # Parse command line args
@@ -150,16 +180,17 @@ def main():
     loglevel = args.loglevel or logging.INFO
     configure_logging(level=loglevel)
 
-    copy_course( args.ntiid,
-                 args.source_host,
-                 args.dest_host,
-                 args.user,
-                 site_library,
-                 args.admin_level,
-                 provider_id = args.provider_id,
-                 start_date = args.start_date,
-                 end_date = args.end_date,
-                 cleanup=args.no_cleanup )
+    copy_course(args.ntiid,
+                args.source_host,
+                args.dest_host,
+                args.user,
+                site_library,
+                args.admin_level,
+                provider_id=args.provider_id,
+                start_date=args.start_date,
+                end_date=args.end_date,
+                cleanup=args.no_cleanup)
 
-if __name__ == '__main__': # pragma: no cover
-        main()
+
+if __name__ == '__main__':  # pragma: no cover
+    main()
