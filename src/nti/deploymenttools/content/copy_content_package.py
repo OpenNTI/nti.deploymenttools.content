895d2c4555264f27c75a53887eaaa435932211d0
Carlos Sanchez <papachoco@gmail.com>
2017-10-13 15:06:16 -0500

    pep8; pylint

--- src/nti/deploymenttools/content/copy_content_package.py
+++ src/nti/deploymenttools/content/copy_content_package.py
@@ -1,59 +1,80 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
 
-from argparse import ArgumentParser
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+import os
+import logging
 from getpass import getpass
+from argparse import ArgumentParser
+
 from requests import exceptions as requests_exceptions
 
 from nti.deploymenttools.content import configure_logging
 from nti.deploymenttools.content import download_rendered_content
 from nti.deploymenttools.content import upload_rendered_content
 
-import logging
-import os
-
-logger = logging.getLogger('nti_copy_content_package')
-logging.captureWarnings(True)
-
 UA_STRING = 'NextThought Content Package Copy Utility'
 
-def copy_content_package(content_ntiid, source_host, dest_host, username, site_library, cleanup=True):
+logger = __import__('logging').getLogger(__name__)
+logging.captureWarnings(True)
+
+
+def _remove_path(path):
+    if path and os.path.exists(path):
+        os.remove(path)
+
+
+def copy_content_package(content_ntiid, source_host, dest_host, username,
+                         site_library, cleanup=True):
     content_archive = None
     try:
-        logger.info("Downloading content package from %s" % (source_host,))
+        logger.info("Downloading content package from %s", source_host)
         password = getpass('Password for %s@%s: ' % (username, source_host))
-        content_archive = download_rendered_content( content_ntiid, source_host, username, password, UA_STRING )
+        content_archive = download_rendered_content(content_ntiid, source_host,
+                                                    username, password, UA_STRING)
 
-        logger.info("Uploading content package to %s" % (dest_host,))
+        logger.info("Uploading content package to %s", dest_host)
         password = getpass('Password for %s@%s: ' % (username, dest_host))
-        content = upload_rendered_content( content_archive, dest_host, username, password, site_library, UA_STRING )
-        logger.info('Successfully uploaded as %s' % (content['Items'].keys()[0],))
+        content = upload_rendered_content(content_archive, dest_host,
+                                          username, password, site_library, UA_STRING)
+        logger.info('Successfully uploaded as %s',
+                    list(content['Items'].keys())[0])
     except requests_exceptions.HTTPError as e:
         logger.error(e)
     finally:
-        # Clean-up
         if cleanup:
-            if content_archive and os.path.exists( content_archive ):
-                os.remove( content_archive )
+            _remove_path(content_archive)
+
 
 def _parse_args():
-    arg_parser = ArgumentParser( description=UA_STRING )
-    arg_parser.add_argument( '-n', '--ntiid', dest='content_ntiid',
-                             help="NTIID of content to copy." )
-    arg_parser.add_argument( '-s', '--source-server', dest='source_host',
-                             help="Source server." )
-    arg_parser.add_argument( '-d', '--dest-server', dest='dest_host',
-                             help="Destination server." )
-    arg_parser.add_argument( '-u', '--user', dest='user',
-                             help="User to authenticate with the server." )
-    arg_parser.add_argument( '--site-library', dest='site_library',
-                             help="Site library to add content to. Defaults to the hostname of the destination server." )
-    arg_parser.add_argument( '-v', '--verbose', dest='loglevel', action='store_const', const=logging.DEBUG,
-                             help="Print debugging logs." )
-    arg_parser.add_argument( '-q', '--quiet', dest='loglevel', action='store_const', const=logging.WARNING,
-                             help="Print warning and error logs only." )
-    arg_parser.add_argument( '--no-cleanup', dest='no_cleanup', action='store_false', default=True,
-                             help="Do not cleanup process files." )
+    arg_parser = ArgumentParser(description=UA_STRING)
+    arg_parser.add_argument('-n', '--ntiid', dest='content_ntiid',
+                            help="NTIID of content to copy.")
+    arg_parser.add_argument('-s', '--source-server', dest='source_host',
+                            help="Source server.")
+    arg_parser.add_argument('-d', '--dest-server', dest='dest_host',
+                            help="Destination server.")
+    arg_parser.add_argument('-u', '--user', dest='user',
+                            help="User to authenticate with the server.")
+    arg_parser.add_argument('--site-library', dest='site_library',
+                            help="Site library to add content to. Defaults to the hostname of the destination server.")
+    arg_parser.add_argument('-v', '--verbose', dest='loglevel', action='store_const',
+                            const=logging.DEBUG,
+                            help="Print debugging logs.")
+    arg_parser.add_argument('-q', '--quiet', dest='loglevel', action='store_const',
+                            const=logging.WARNING,
+                            help="Print warning and error logs only.")
+    arg_parser.add_argument('--no-cleanup', dest='no_cleanup', action='store_false',
+                            default=True,
+                            help="Do not cleanup process files.")
     return arg_parser.parse_args()
+
 
 def main():
     # Parse command line args
@@ -64,7 +85,10 @@ def main():
     loglevel = args.loglevel or logging.INFO
     configure_logging(level=loglevel)
 
-    copy_content_package( args.content_ntiid, args.source_host, args.dest_host, args.user, site_library, cleanup=args.no_cleanup )
+    copy_content_package(args.content_ntiid, args.source_host,
+                         args.dest_host, args.user, site_library,
+                         cleanup=args.no_cleanup)
 
-if __name__ == '__main__': # pragma: no cover
-        main()
+
+if __name__ == '__main__':  # pragma: no cover
+    main()
