987a6a5ddd69936f31d07a3301ef1b0b0bfece7a
Sean <sean.jones@nextthought.com>
2017-11-11 12:16:07 -0600

    Revert "Minor"

--- src/nti/deploymenttools/content/import_course_bundle.py
+++ src/nti/deploymenttools/content/import_course_bundle.py
@@ -9,9 +9,11 @@ from __future__ import print_function
 from __future__ import absolute_import
 
 import os
-import getpass
 import logging
-import argparse
+from getpass import getpass
+from argparse import ArgumentParser
+
+import requests
 
 from nti.deploymenttools.content import import_course
 from nti.deploymenttools.content import configure_logging
@@ -19,10 +21,11 @@ from nti.deploymenttools.content import configure_logging
 UA_STRING = 'NextThought Course Import Utility'
 
 logger = __import__('logging').getLogger(__name__)
+logging.captureWarnings(True)
 
 
-def _parse_args(args=None):
-    arg_parser = argparse.ArgumentParser(description=UA_STRING)
+def _parse_args():
+    arg_parser = ArgumentParser(description=UA_STRING)
     arg_parser.add_argument('coursepath', help="Course archive to import")
     arg_parser.add_argument('-d', '--dest-server', dest='dest_host',
                             help="Destination server.")
@@ -38,29 +41,32 @@ def _parse_args(args=None):
                             help="Print debugging logs.")
     arg_parser.add_argument('-q', '--quiet', dest='loglevel', action='store_const', const=logging.WARNING,
                             help="Print warning and error logs only.")
-    return arg_parser.parse_args(args)
+    return arg_parser.parse_args()
 
 
-def main(args=None):
-    args = _parse_args(args)
-    archive = os.path.abspath(os.path.expanduser(args.coursepath))
-    if not os.path.exists(archive) or not not os.path.isfile(archive):
-        raise IOError("Invalid course archive")
+def main():
+    # Parse command line args
+    args = _parse_args()
+    course_archive = os.path.abspath(os.path.expanduser(args.coursepath))
 
     site_library = args.site_library or args.dest_host
 
     loglevel = args.loglevel or logging.INFO
     configure_logging(level=loglevel)
 
-    msg = 'Password for %s@%s: ' % (args.user, args.dest_host)
-    password = getpass.getpass(msg)
-    logger.info("Importing course from %s to %s",
-                archive, args.dest_host)
-    course = import_course(archive, args.dest_host, args.user,
-                           password, site_library, args.admin_level,
-                           args.provider_id, UA_STRING)
-    logger.info('Course imported sucessfully as %s.' %
-                (course['Course']['NTIID'],))
+    try:
+        password = getpass('Password for %s@%s: ' %
+                           (args.user, args.dest_host))
+        logger.info("Importing course from %s to %s",
+                    course_archive, args.dest_host)
+        course = import_course(course_archive, args.dest_host, args.user,
+                               password, site_library, args.admin_level,
+                               args.provider_id, UA_STRING)
+        logger.info('Course imported sucessfully as %s.' %
+                    (course['Course']['NTIID'],))
+
+    except requests.exceptions.HTTPError as e:
+        logger.error(e)
 
 
 if __name__ == '__main__':  # pragma: no cover
